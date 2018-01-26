cbb5429041564a265dd82647785fd0853dd38535
Andrew Ehlers <asehlers17@gmail.com>
2018-01-26 13:08:39 -0500

    basic file structure setup

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var db = requiere("./models");
+
+var PORT = process.env.PORT || 3000
+var app = express();
+
+db.sequelize.sync().then(function()
+{
+    app.listen(PORT, function()
+    {
+        console.log("Listening on port %s", PORT);
+    });
+});
