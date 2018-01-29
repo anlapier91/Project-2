a934036889158afac6b5e0caba3a3b5fb8e51ea8
Andrew Ehlers <asehlers17@gmail.com>
2018-01-29 14:27:59 -0500

    basic funtionality for two tables

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3000
 var app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
